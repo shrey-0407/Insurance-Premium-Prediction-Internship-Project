c307f0817fbcc7444ca35eb866c6c334342d8f6f
shrey <shreyash966977@gmail.com>
2022-08-08 12:55:57 +0530

    logger and exception

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+
+import sys
+from insurance.logger import logging
+from insurance.exception import InsuranceException
 app=Flask(__name__)
+
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        insurance = InsuranceException(e,sys)
+        logging.info(insurance.error_message)
+        logging.info("We are testing logging module")
     return "CI CD pipeline has been established."
-
-
-if __name__=="__main__":
-    app.run(debug=True)
